e797703c2e6a16ac0595fa6c54037ec1780d21e6
Dyarlen Iber <LATAM-EIG\diber@endudev2506.latam-eig.com>
2019-07-29 13:30:01 -0300

    Redux and Redux Sauce configuration

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Creators as RepositoriesActions } from '../../store/ducks/repositories';
 
 import { Container, Navbar } from './styles';
 
-function Header() {
+function Header({ searchRepositoryByUser }) {
+  const handleSearchClick = (e, searchAction) => {
+    e.preventDefault();
+    searchAction();
+  };
+
   return (
     <Container>
       <Navbar>
         <h1>Logo</h1>
-        <input type="text" placeholder="Adicionar" />
+        <form>
+          <input type="text" placeholder="Adicionar" />
+          <button type="submit" onClick={e => handleSearchClick(e, searchRepositoryByUser)}>Pesquisar</button>
+        </form>
         <h1>menu</h1>
       </Navbar>
     </Container>
   );
 }
 
-export default Header;
+const mapDispatchToProps = dispatch => bindActionCreators(RepositoriesActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(Header);
---
+++ src/store/ducks/repositories.js
@@ -0,0 +1,50 @@
+import { createActions, createReducer } from 'reduxsauce';
+
+/**
+ * Types & creators
+ */
+export const { Types, Creators } = createActions({
+  searchRepositoryByUser: ['user'],
+  searchRepositoryByLanguage: ['language'],
+  searchRepositoryByYear: ['year'],
+  createRepository: ['repository'],
+});
+
+/**
+ * Handlers
+ */
+const INITIAL_STATE = [];
+
+const searchRepositoryByUser = (state = INITIAL_STATE, action) => {
+    console.log('searchRepositoryByUser');
+    console.log(action);
+    return state;
+};
+
+const searchRepositoryByLanguage = (state = INITIAL_STATE, action) => {
+    console.log('searchRepositoryByLanguage');
+    console.log(action);
+    return state;
+};
+
+const searchRepositoryByYear = (state = INITIAL_STATE, action) => {
+    console.log('searchRepositoryByYear');
+    console.log(action);
+    return state;
+};
+
+const createRepository = (state = INITIAL_STATE, action) => {
+    console.log('createRepository');
+    console.log(action);
+    return state;
+};
+
+/**
+ * Reducer
+ */
+export default createReducer(INITIAL_STATE, {
+  [Types.SEARCH_REPOSITORY_BY_USER]: searchRepositoryByUser,
+  [Types.SEARCH_REPOSITORY_BY_LANGUAGE]: searchRepositoryByLanguage,
+  [Types.SEARCH_REPOSITORY_BY_YEAR]: searchRepositoryByYear,
+  [Types.CREATE_REPOSITORY]: createRepository,
+});
