6512d8f9e481eecbdfd99d106ef6e27e19c83aaf
André Luiz <andreluizr@gmail.com>
2019-08-03 10:06:58 -0300

    A

--- src/store/ducks/repositories.js
+++ src/store/ducks/repositories.js
@@ -1,13 +1,13 @@
-import { createActions, createReducer } from 'reduxsauce';
+import { createActions, createReducer } from "reduxsauce";
 
 /**
  * Types & creators
  */
 export const { Types, Creators } = createActions({
-  searchRepositoryByUser: ['user'],
-  searchRepositoryByLanguage: ['language'],
-  searchRepositoryByYear: ['year'],
-  createRepository: ['repository'],
+  searchRepositoryByUser: ["user"],
+  searchRepositoryByLanguage: ["language"],
+  searchRepositoryByYear: ["year"],
+  createRepository: ["repository"]
 });
 
 /**
@@ -16,27 +16,27 @@ export const { Types, Creators } = createActions({
 const INITIAL_STATE = [];
 
 const searchRepositoryByUser = (state = INITIAL_STATE, action) => {
-    console.log('searchRepositoryByUser');
-    console.log(action);
-    return state;
+  console.log("searchRepositoryByUser");
+  console.log(action);
+  return state;
 };
 
 const searchRepositoryByLanguage = (state = INITIAL_STATE, action) => {
-    console.log('searchRepositoryByLanguage');
-    console.log(action);
-    return state;
+  console.log("searchRepositoryByLanguage");
+  console.log(action);
+  return state;
 };
 
 const searchRepositoryByYear = (state = INITIAL_STATE, action) => {
-    console.log('searchRepositoryByYear');
-    console.log(action);
-    return state;
+  console.log("searchRepositoryByYear");
+  console.log(action);
+  return state;
 };
 
 const createRepository = (state = INITIAL_STATE, action) => {
-    console.log('createRepository');
-    console.log(action);
-    return state;
+  console.log("createRepository");
+  console.log(action);
+  return state;
 };
 
 /**
@@ -46,5 +46,5 @@ export default createReducer(INITIAL_STATE, {
   [Types.SEARCH_REPOSITORY_BY_USER]: searchRepositoryByUser,
   [Types.SEARCH_REPOSITORY_BY_LANGUAGE]: searchRepositoryByLanguage,
   [Types.SEARCH_REPOSITORY_BY_YEAR]: searchRepositoryByYear,
-  [Types.CREATE_REPOSITORY]: createRepository,
+  [Types.CREATE_REPOSITORY]: createRepository
 });
