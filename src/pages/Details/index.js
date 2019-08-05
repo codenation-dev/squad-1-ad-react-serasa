c9f30c288d841bed0ddad66f15689aa22d73fa5b
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2019-08-05 19:22:57 -0300

    'Details'

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import apiAxios from '../../services/apiAxios';
+
+import {
+  Container, Info, Owner, RepoList,
+} from './styles';
 
 class Details extends Component {
   static propTypes = {
@@ -28,7 +33,50 @@ class Details extends Component {
     const { user, repos } = this.state;
     return (
       <div>
-        <h1>Details</h1>
+        {user ? (
+          <Container>
+            <Owner>
+              <Link to="/">Voltar aos repositorios</Link>
+              <img src={user.avatar_url} alt={user.login} />
+              <h1>{user.name}</h1>
+              <p>{user.bio}</p>
+            </Owner>
+            <RepoList>
+              {repos.map(repo => (
+                <li key={String(repo.id)}>
+                  <div>
+                    <h2>
+                      <a href={repo.html_url}>{repo.name}</a>
+                    </h2>
+                    <Info>
+                      <strong>{repo.forks_count}</strong>
+                      <p>Forks </p>
+                    </Info>
+
+                    <Info>
+                      <strong>{repo.stargazers_count}</strong>
+                      <p>Stars </p>
+                    </Info>
+
+                    <Info>
+                      <strong>{repo.open_issues_count}</strong>
+                      <p>Issues </p>
+                    </Info>
+
+                    <Info>
+                      <strong>{repo.created_at}</strong>
+                      <p>Created </p>
+                    </Info>
+                  </div>
+                </li>
+              ))}
+            </RepoList>
+          </Container>
+        ) : (
+          <Container>
+            <h1>No show usuario</h1>
+          </Container>
+        )}
       </div>
     );
   }
--- src/store/sagas/users.js
+++ src/store/sagas/users.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import apiAxios from '../../services/apiAxios';
@@ -8,6 +8,11 @@ import UserActions from '../ducks/users';
 export function* addUser(action) {
   try {
     const response = yield call(apiAxios.get, action.user);
+
+    const isDuplicated = yield select(state => state.users.data.find(user => user.id === response.data.id));
+    if (isDuplicated) {
+      throw toast.error('Usuario ja adicionado');
+    }
 
     yield put(UserActions.getUserSuccess(response.data));
   } catch (error) {
