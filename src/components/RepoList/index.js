bf39521dd0fd70d57909e42098c56409367cc4db
Dyarlen Iber <dyarlen1@gmail.com>
2019-08-09 00:13:47 -0300

    Repositories by year update

--- src/components/RepoList/index.js
+++ src/components/RepoList/index.js
@@ -6,49 +6,48 @@ import { Info, Container } from './styles';
 
 export default function RepoList({ repos }) {
   return (
-    <Container>
-      {repos.map(repo => (
-        <li key={repo.id}>
-          <div>
-            <h2>
-              <a href={repo.html_url}>{repo.name}</a>
-            </h2>
-            <Info>
-              <strong>{repo.forks_count}</strong>
-              <p>Forks </p>
-            </Info>
+    <div>
+      {repos.map((reposByYear, year) => (
+        <Container key={year}>
+          <h1>
+            {year}
+          </h1>
+          {reposByYear.map(repo => (
+            <li key={repo.id}>
+              <div>
+                <h2>
+                  <a href={repo.html_url}>{repo.name}</a>
+                </h2>
+                <Info>
+                  <strong>{repo.forks_count}</strong>
+                  <p>Forks </p>
+                </Info>
 
-            <Info>
-              <strong>{repo.stargazers_count}</strong>
-              <p>Stars </p>
-            </Info>
+                <Info>
+                  <strong>{repo.stargazers_count}</strong>
+                  <p>Stars </p>
+                </Info>
 
-            <Info>
-              <strong>{repo.open_issues_count}</strong>
-              <p>Issues </p>
-            </Info>
+                <Info>
+                  <strong>{repo.open_issues_count}</strong>
+                  <p>Issues </p>
+                </Info>
 
-            <Info>
-              <strong>{moment(repo.created_at).format('L')}</strong>
-              <p>Created </p>
-            </Info>
-          </div>
-        </li>
+                <Info>
+                  <strong>{moment(repo.created_at).format('L')}</strong>
+                  <p>Created </p>
+                </Info>
+              </div>
+            </li>
+          ))}
+        </Container>
       ))}
-    </Container>
+    </div>
   );
 }
 
 RepoList.propTypes = {
   repos: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      html_url: PropTypes.string,
-      stargazers_count: PropTypes.number,
-      open_issues_count: PropTypes.number,
-      forks_count: PropTypes.number,
-      created_at: PropTypes.string,
-    }),
+    PropTypes.shape(PropTypes.number),
   ).isRequired,
 };
